18b050d1c3cc6793afa46157555f62406f9a7580
Lykkss <quaglierilisa3@gmail.com>
2025-02-07 09:29:51 +0100

    Implement user profile management with viewing and editing features

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,21 +1,49 @@
-<?php 
+<?php
 
 namespace App\Controller;
 
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/profile')]
+#[Route('/profil')]
 class UserController extends AbstractController
 {
-    #[Route('/', name: 'profile')]
-    #[IsGranted('ROLE_USER')]
-    public function index(): Response
+    #[Route('/', name: 'profil_show', methods: ['GET'])]
+    public function show(): Response
     {
-        return $this->render('user/index.html.twig', [
-            'user' => $this->getUser(),
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('user/profil.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/edit', name: 'profil_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Profil mis à jour avec succès.');
+            return $this->redirectToRoute('profil_show');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
